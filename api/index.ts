b1ea44afe96c201c74b860997487ee11d7008b9e
Jollyer <jollyer@outlook.kr>
2023-05-04 12:40:16 +0900

    inhouse

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,7 @@
 export * from './user/user.response';
 export * from './user/user.request';
 export * from './stock/stock.request';
+export * from './inhouse/location.request';
+export * from './inhouse/location.response';
+export * from './inhouse/warehouse.request';
+export * from './inhouse/warehouse.response';
