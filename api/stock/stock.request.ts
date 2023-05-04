139daa32d5e86eae4186079a1bf466b8166f75f3
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-05-04 16:50:17 +0900

    자사 재고그룹 조회 필드추가

--- api/stock/stock.request.ts
+++ api/stock/stock.request.ts
@@ -1,7 +1,21 @@
 import { PaginationQuery } from "../../models/pagination";
 
-/** 재고그룹 목록 요청 */
+/** 자사 재고그룹 목록 요청 */
 export interface StockGroupListQuery extends PaginationQuery { }
+
+/** 자사 재고목록 요청 */
+export interface StockListQuery {
+  warehouseId?: number;
+  productId: number;
+  packagingId: number;
+  grammage: number;
+  sizeX: number;
+  sizeY?: number;
+  paperColorGroupId?: number;
+  paperColorId?: number;
+  paperPatternId?: number;
+  paperCertId?: number;
+}
 
 /** 재고 생성 (신규 등록) */
 export interface StockCreateRequest {
