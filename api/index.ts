2935f9305ec78598fe77ffacbc2b762d8d0d9be1
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-05-04 11:49:21 +0900

    재고관련 api, model 추가

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,3 @@
 export * from './user/user.response';
 export * from './user/user.request';
+export * from './stock/stock.request';
---
+++ api/stock/stock.request.ts
@@ -0,0 +1,20 @@
+import { PaginationQuery } from "src/@shared/models/pagination";
+
+/** 재고그룹 목록 요청 */
+export interface StockGroupListQuery extends PaginationQuery { }
+
+/** 재고 생성 (신규 등록) */
+export interface StockCreateRequest {
+    warehouseId: number | null;
+    productId: number;
+    packagingId: number;
+    grammage: number;
+    sizeX: number;
+    sizeY?: number;
+    paperColorGroupId?: number | null;
+    paperColorId?: number | null;
+    paperPatternId?: number | null;
+    paperCertId?: number | null;
+    quantity: number;
+}
+
