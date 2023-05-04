d41c3a22cd0c9bae2f3748f86d178b4a16dcda8d
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-05-04 13:21:48 +0900

    재고신규등록록 API

--- src/modules/stock/api/stock.controller.ts
+++ src/modules/stock/api/stock.controller.ts
@@ -1,12 +1,59 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Post, Request, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "src/modules/auth/auth.guard";
+import { AuthType } from "src/modules/auth/auth.type";
+import { StockChangeService } from "../service/stock-change.service";
 import { StockCreateRequestDto } from "./dto/stock.request";
+import { ulid } from 'ulid';
 
 @Controller('/stock')
 export class StockController {
+    constructor(
+        private readonly stockChangeService: StockChangeService,
+    ) { }
 
     @Post()
-    @UsePipes(new ValidationPipe({ transform: true }))
-    async create(@Body() dto: StockCreateRequestDto) {
-        console.log(dto);
+    @UseGuards(AuthGuard)
+    async create(@Request() req: AuthType, @Body() dto: StockCreateRequestDto): Promise<any> {
+        await this.stockChangeService.create({
+            serial: ulid(),
+            company: {
+                connect: {
+                    id: req.user.companyId,
+                }
+            },
+            product: {
+                connect: {
+                    id: dto.productId,
+                }
+            },
+            grammage: dto.grammage,
+            sizeX: dto.sizeX,
+            sizeY: dto.sizeY,
+            packaging: {
+                connect: {
+                    id: dto.packagingId,
+                }
+            },
+            paperColorGroup: dto.paperColorGroupId ? {
+                connect: {
+                    id: dto.paperColorGroupId,
+                }
+            } : undefined,
+            paperColor: dto.paperColorId ? {
+                connect: {
+                    id: dto.paperColorId,
+                }
+            } : undefined,
+            paperPattern: dto.paperPatternId ? {
+                connect: {
+                    id: dto.paperPatternId,
+                }
+            } : undefined,
+            paperCert: dto.paperCertId ? {
+                connect: {
+                    id: dto.paperCertId,
+                }
+            } : undefined,
+        }, dto.quantity);
     }
 }
