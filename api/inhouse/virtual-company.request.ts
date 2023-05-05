7162dce6b2d70c7133648a7afa685b78dd8096a1
jollyer <jollyer@outlook.kr>
2023-05-05 11:55:15 +0900

    회사 송장코드, 대표자, 주소 등 속성 추가

--- api/inhouse/virtual-company.request.ts
+++ api/inhouse/virtual-company.request.ts
@@ -4,6 +4,10 @@ export type VirtualCompanyListQuery = PaginationQuery;
 
 export interface VirtualCompanyCreateRequest {
   businessName: string;
+  companyRegistrationNumber: string;
+  invoiceCode: string;
+  representative: string;
+  address: string;
   phoneNo: string;
   faxNo: string;
   email: string;
