d185836ce693ed7c3637edd39b625fa1a6c112a5
jollyer <jollyer@outlook.kr>
2023-05-05 12:27:59 +0900

    회사 속성 모델 수정

--- models/company.ts
+++ models/company.ts
@@ -2,6 +2,9 @@ export default interface Company {
   id: number;
   businessName: string;
   companyRegistrationNumber: string;
+  invoiceCode: string;
+  representative: string;
+  address: string;
   phoneNo: string;
   faxNo: string;
   email: string;
