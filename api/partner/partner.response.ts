76014de18b13f2f58f03088aeffeae3b078b9554
권진오 <kjo1333@gmail.com>
2023-05-09 16:15:51 +0900

    파트너 수정

--- api/partner/partner.response.ts
+++ api/partner/partner.response.ts
@@ -1,5 +1,3 @@
 import { Partner } from '../../models';
-import { PaginationQuery } from '../../models/pagination';
 
-export type PartnerResponse = Omit<Partner, 'id'> &
-  PaginationQuery & { partnerId: number };
+export type PartnerResponse = Partner;
--- models/partner.ts
+++ models/partner.ts
@@ -1,15 +1,5 @@
-import { Company, Entity } from ".";
 
-export default interface Partner extends Entity {
-    id: number;
-    ceoName: string;
-    ceoPhoneNumber: string;
-    companyRegistrationNumber: string;
-    representativeNumber: string;
-    partnerNickName: string;
-    partnerName: string;
-    memo: string;
-    isDeleted: boolean;
-    companyId: number;
-    company: Company;
+export default interface Partner {
+  partnerId: number;
+  partnerNickName: string;
 }
