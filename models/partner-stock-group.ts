c276137eb3cf2818a5c05ced55ad88da1f26ed46
Jollyer <jollyer@outlook.kr>
2023-05-11 16:43:12 +0900

    .

--- models/partner-stock-group.ts
+++ models/partner-stock-group.ts
@@ -1,25 +1,24 @@
-import OrderStock from "./order-stock";
-import Packaging from "./packaging";
-import PaperCert from "./paper-cert";
-import PaperColor from "./paper-color";
-import PaperColorGroup from "./paper-color-group";
-import PaperPattern from "./paper-pattern";
-import Product from "./product";
-import Warehouse from "./warehouse";
-
+import OrderStockBase from './order-stock-base';
+import Packaging from './packaging';
+import PaperCert from './paper-cert';
+import PaperColor from './paper-color';
+import PaperColorGroup from './paper-color-group';
+import PaperPattern from './paper-pattern';
+import Product from './product';
+import Warehouse from './warehouse';
 
 export default interface PartnerStockGroup {
-    warehouse: Warehouse;
-    product: Product;
-    packaging: Packaging;
-    grammage: number;
-    sizeX: number;
-    sizeY: number;
-    paperColorGroup: PaperColorGroup | null;
-    paperColor: PaperColor | null;
-    paperPattern: PaperPattern | null;
-    paperCert: PaperCert | null;
-    orderStock: OrderStock | null;
-    totalQuantity: number;
-    availableQuantity: number;
+  warehouse: Warehouse;
+  product: Product;
+  packaging: Packaging;
+  grammage: number;
+  sizeX: number;
+  sizeY: number;
+  paperColorGroup: PaperColorGroup | null;
+  paperColor: PaperColor | null;
+  paperPattern: PaperPattern | null;
+  paperCert: PaperCert | null;
+  orderStock: OrderStockBase | null;
+  totalQuantity: number;
+  availableQuantity: number;
 }
