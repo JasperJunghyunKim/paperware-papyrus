99670fe026c333b3ae3eddb44d374e4e4da1711a
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-05-11 17:01:31 +0900

    고시가 상세

--- api/inhouse/official-price.response.ts
+++ api/inhouse/official-price.response.ts
@@ -3,3 +3,6 @@ import { PaginationResponse } from "../../models/pagination";
 
 /** 고시가 목록 */
 export type OfficialPriceListResponse = PaginationResponse<OfficialPriceCondition>;
+
+/** 고시가 상세 */
+export type OfficialPriceResponse = OfficialPriceCondition;
