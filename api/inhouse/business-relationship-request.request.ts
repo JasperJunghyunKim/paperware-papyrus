1ad89c550bff694b79c789aaf23d2e53a9307843
Jollyer <jollyer@outlook.kr>
2023-05-16 17:18:16 +0900

    .

--- api/inhouse/business-relationship-request.request.ts
+++ api/inhouse/business-relationship-request.request.ts
@@ -2,12 +2,6 @@ import { PaginationQuery } from '../../models/pagination';
 
 export type BusinessRelationshipRequestListQuery = PaginationQuery;
 
-export interface BusinessRelationshipRequestCreateRequest {
-  companyId: number;
-  isPurchase: boolean;
-  isSales: boolean;
-}
-
 export interface BusinessRelationshipRequestAcceptRequest {
   companyId: number;
 }
