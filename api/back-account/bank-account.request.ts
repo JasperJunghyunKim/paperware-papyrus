f6c35d93f2e374ac6d978d15dc1b27f6153e5ef9
권진오 <kwonjin-o@gwonjin-oui-Macmini.local>
2023-05-17 10:30:01 +0900

    카드 계좌 완료

--- api/back-account/bank-account.request.ts
+++ api/back-account/bank-account.request.ts
@@ -2,5 +2,5 @@ import BankAccount from '../..//models/bank-account';
 import { PaginationQuery } from '../../models/pagination';
 
 export type BankAccountQuery = PaginationQuery;
-export type BankAccountCreateRequest = BankAccount;
+export type BankAccountCreateRequest = Omit<BankAccount, 'accountId'>;
 export type BankAccountUpdateRequest = Pick<BankAccount, 'accountName'>;
