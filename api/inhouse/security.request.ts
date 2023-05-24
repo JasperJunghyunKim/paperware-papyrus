b09067994be7ce2b88d81dc6a2d4162ae9524801
권진오 <kjo1333@gmail.com>
2023-05-24 13:47:08 +0900

    모델 수정

--- api/inhouse/security.request.ts
+++ api/inhouse/security.request.ts
@@ -2,6 +2,6 @@ import Security from '../../models/security';
 import { PaginationQuery } from '../../models/pagination';
 
 export type SecurityQuery = PaginationQuery;
-export type SecurityCreateRequest = Omit<Security, 'securityId'>;
-export type SecurityUpdateRequest = Omit<Security, 'securityId' | 'securityStatus'>;
+export type SecurityCreateRequest = Omit<Security, 'securityId' | 'drawedStatus' | 'securityStatus'>;
+export type SecurityUpdateRequest = Omit<Security, 'securityId'>;
 export type SecurityUpdateStatusRequest = Pick<Security, 'memo' | 'securityStatus'>;
