6ff0f0975bccbceca6f2c69c8a91984b027188e7
jollyer <jollyer@outlook.kr>
2023-08-03 15:58:28 +0900

    prisma global instance

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
 export default prisma;
