a21d81402ea7810f427db0bf85f5420b4bbdf127
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-08-04 11:36:38 +0900

    팝빌 연동해제

--- src/app/api/popbill/member/[id]/route.ts
+++ src/app/api/popbill/member/[id]/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 import { ConflictError, InternalServerError, NotFoundError } from "@/lib/server/error";
 import { handleApi } from "@/lib/server/handler";
-import { PopbillGetCorpInfoResponse } from "@/lib/types/popbill";
+import { PopbillDefaultResponse, PopbillGetCorpInfoResponse } from "@/lib/types/popbill";
 import axios from "axios";
 import { z } from "zod";
 
@@ -51,17 +51,46 @@ export const GET = handleApi<GetPopbillCompanyInfoReseponse>(async (req, context
 export const DELETE = handleApi(async (req, context) => {
     const params = await paramsSchema.parseAsync(context.params);
 
-    const company = await prisma.company.findFirst({
-        where: {
-            id: params.id,
-            managedById: null,
+    await prisma.$transaction(async tx => {
+        const company = await tx.company.findFirst({
+            where: {
+                id: params.id,
+                managedById: null,
+            }
+        });
+        if (!company) throw new NotFoundError('존재하지 않는 고객사');
+        if (company.isDeleted) throw new ConflictError('이미 탈퇴 처리된 고객사');
+    
+        if(!company.popbillId) throw new ConflictError('팝빌 연동되어 있지 않는 고객사');
+    
+        const result = await new Promise((res, rej) => {
+            axios.post(`${process.env.POPBILL_API_URL}/TaxinvoiceService/QuitMember`, {
+                companyRegistrationNumber: company.companyRegistrationNumber,
+                reason: '서비스 고객사 연동해제',
+                popbillId: company.popbillId,
+            }
+            ).then(result => {
+                res(result.data)
+            }).catch(err => {
+                console.log(err);
+                rej(err)
+            })
+        });
+    
+        if (result instanceof Error) {
+            throw new InternalServerError(result.message);
         }
-    });
-    if (!company) throw new NotFoundError('존재하지 않는 고객사');
-    if (company.isDeleted) throw new ConflictError('이미 탈퇴 처리된 고객사');
 
-    if(!company.popbillId) throw new ConflictError('팝빌 연동되어 있지 않는 고객사');
-  
+        await tx.company.update({
+            where: {
+                id: company.id,
+            },
+            data: {
+                popbillId: null,
+            }
+        });
+    });
+
   });
 
 export type GetPopbillCompanyInfoReseponse = {
